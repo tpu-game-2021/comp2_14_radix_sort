2f83d5350d020b665c54d737067ff64a1170636f
g2127043@st.t-kougei.ac.jp <g2127043@st.t-kougei.ac.jp>
2021-12-19 03:30:11 +0900

    テストを通せませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -8,6 +8,38 @@
 // 基数は256で実装せよ
 bool radix_sort(item* begin, const item* end)
 {
+	if (begin == NULL || end == NULL) return false;
+	if (begin > end) return false;
 	// ToDo: 基数ソートを実装する
-	return false;
+
+	int max = begin->key;
+	int size = end - begin;
+	for (item* it = begin; it != end; it++)
+		if (max < it->key) max = it->key; // 最大値の検索
+
+	for (int m = 1; m < max; m *= 256) {
+		int count[256] = { 0 };
+		for (item* it1 = begin; it1 != end; it1++) // バケツごとの要素数
+		{
+			int num = it1->key / m % 256;
+			count[num]++;
+		}
+
+		int l = 0;
+		for (int i = 0; i < 256; i++) {
+			item* it1 = begin;
+			int  k = 0;
+			for (int j = 0, k = 0; k < count[i] && j < size; j++, it1++) {
+				if (it1->key / m % 256 == i) {
+					item temp = begin[l];
+					begin[l] = *it1;
+					*it1 = temp;
+					l++;
+					k++;
+				}
+			}
+		}
+	}
+	return true;
 }
+
