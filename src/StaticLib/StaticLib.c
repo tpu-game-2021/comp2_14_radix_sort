5dfbd18c5e00cd1089fc99c97a0a4d7fa13933ac
Fumirin16 <tsubasa030116@outlook.jp>
2021-12-20 00:11:35 +0900

    バブルソートとの比較だけが出来ませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -2,12 +2,56 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
+#include<stdlib.h>
+#include "malloc.h"
 
-
-// 基数ソート(引数が不適切であればfalseを返す)
-// 基数は256で実装せよ
 bool radix_sort(item* begin, const item* end)
 {
-	// ToDo: 基数ソートを実装する
-	return false;
+    int i, j, k;
+
+    if (end == NULL || begin == NULL || end < begin)
+    {
+        return false;
+    }
+    if (end - begin == 1)
+    {
+        return true;
+    }
+
+    int Max = begin[0].key;
+    int num = end - begin;
+    int m = 1;
+    int* secure = (int*)malloc(sizeof(int) * num); 
+    item* temp = (item*)malloc(sizeof(item) * num);
+
+    while (m <= Max) 
+    {
+        for (i = 0; i < num; i++)
+        {
+            secure[i] = (begin[i].key / m) % 256;   /* 基数を取り出し rad[i] に保存 */
+        }
+
+        k = 0;                          /* 配列の添字として使う */
+        for (i = 0; i <= 256; i++)/* 基数は0から9 */
+        {
+            for (j = 0; j < num; j++)
+            {
+                if (secure[j] == i)/* 基数の小さいものから */
+                {
+                    temp[k++] = begin[j];      /* y[ ] にコピー */
+                }
+            }
+        }
+
+        for (i = 0; i < num; i++)
+        {
+            begin[i] = temp[i];                /* x[ ] にコピーし直す */
+        }
+
+        m *= 256;                        /*  基数を取り出す桁を一つ上げる */
+    }
+
+    free(secure);
+    free(temp);
+	return true;
 }
