256886db25260c5cb545fe47f06b474170eee452
Nanachi9999 <nnc.dev2021@gmail.com>
2021-12-19 18:22:59 +0900

    テストを通せませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -8,6 +8,32 @@
 // 基数は256で実装せよ
 bool radix_sort(item* begin, const item* end)
 {
+	if (begin == NULL)
+		return false;
+	if (end == NULL)
+		return false;
+
+	int n = (end - begin);
+
+	int i,j;
+	bucket buckets[256];
+
+	for (i = 0; i < 256; i++) 
+	{
+		for (j = 0; j < n; j++)
+		{
+			switch(begin[j] % (10^(i + 1))
+			{
+			default:
+				break;
+			}
+		}
+
+	
+	}
+
+
+
 	// ToDo: 基数ソートを実装する
 	return false;
 }
--- src/include/lib_func.h
+++ src/include/lib_func.h
@@ -13,6 +13,21 @@ extern "C" {
 		char value[256];
 	}item;
 
+	typedef struct bucket_
+	{
+		int zero[256];
+		int one[256];
+		int two[256];
+		int three[256];
+		int four[256];
+		int five[256];
+		int six[256];
+		int seven[256];
+		int eight[256];
+		int nine[256];
+
+	}bucket;
+
 	// 基数ソート(引数が不適切であればfalseを返す)
 	// 基数は256で実装せよ
 	bool radix_sort(item *begin, const item* end);
