9e358446867c5f2466732cd2b5ab1013f57a18b8
umee113 <mayu2943@icloud.com>
2021-12-20 01:55:52 +0900

    変更できませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,5 +9,58 @@
 bool radix_sort(item* begin, const item* end)
 {
 	// ToDo: 基数ソートを実装する
-	return false;
+
+	int i, j, k;
+	int m = 1;
+
+	if (end < begin)
+		return false;
+	if(end==NULL||begin==NULL)
+		return false;
+	if (end - begin == 1)
+		return false;
+
+	int num = end - begin;
+	int max = begin[0].key;
+
+	int* rad = (int*)malloc(sizeof(int) * num);
+	item* temp = (item*)malloc(sizeof(item) * num);
+
+
+	for (int i = 0; i < num; i++)
+	{
+		if (begin[i].key > max)
+		{
+			max = begin[i].key;
+		}	
+	}
+	return max;
+
+
+	while (m <=max)
+	{
+		for (int i = 0; i < num; i++)
+			rad[i] =( begin[i].key/m) % 256;
+
+		int k = 0;
+		for (int i = 0; i < 256; i++)
+		{
+			for (int j = 0; j < 256; j++)
+			{
+				if (rad[j] == i)
+					temp[k++] = begin[j];
+			}
+		}
+		for (int i = 0; i < num; i++)
+			begin[i] = temp[i];
+
+		m *= 256;
+
+	}
+
+	free(rad);
+	free(temp);
+
+
+	return true;
 }
