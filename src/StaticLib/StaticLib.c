9ce48b96ae30fbede38a0bb17b4ceedce5fba110
ryouta <ryouta32ryouta@gmail.com>
2021-12-19 19:42:03 +0900

    変更

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,5 +9,45 @@
 bool radix_sort(item* begin, const item* end)
 {
 	// ToDo: 基数ソートを実装する
-	return false;
+	if (begin == NULL || end == NULL)
+		return false;
+	if (end < begin)
+		return false;
+	if (end - begin == 1)
+		return true;
+
+	int num = end - begin;
+	int rank=1;
+	int max=begin[0].key;
+	int k;
+    int rad[256];
+	item tempo[256];
+
+	for (int i = 0; i < num; i++) {
+		if (begin[i].key > max) {
+			max = begin[i].key;
+		}
+	}
+
+	while (rank<=max)
+	{
+		for (int i = 0; i < num; i++) {
+			rad[i] = (begin[i].key / rank) % 10;
+		}
+
+		k = 0;
+		for (int i = 0; i < 256; i++) {
+			for (int j = 0; j < num; j++) {
+				if (rad[j] == i) {
+					tempo[k++] = begin[j];
+				}
+			}
+		}
+
+		for (int i = 0; i < num; i++)
+			begin[i] = tempo[i];
+
+		rank *= 10;
+	}
+	return true;
 }
